07b8093e78301c50e04ebb5326379ed14a910289
AMIT KUMAR <46942263+ak78158@users.noreply.github.com>
2020-12-18 22:57:31 +0530

    created Search app

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+// $('.search-button').click(function(){
+//     alert("Button Clicked");
+// })
+
+var displayWikipediaData = function() {
+    
+    var $linksElement = $('#links');
+    var searchString = $('#searchString').val();
+    var wikipediaUrl= "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchString + "&format=json&callback=?";
+    $.ajax({
+        url: wikipediaUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function(res){
+            var linkLists = res[1];
+            linkLists.forEach(function(item){
+                var url = 'https://en.wikipedia.org/wiki/' + item;
+                $linksElement.append('<li><a href="'+ url +'">'+item + '</a></li>');
+                return url;
+            })
+        }
+    });
+    return false;
+};
+
+$('#form').submit(displayWikipediaData);
